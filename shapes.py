851109888d05377292ed0d8e4206048a6706c404
Aarti Srinivasan <aartisrinivasan83@gmail.com>
2021-05-02 10:58:27 -0700

    Add files via upload

---
+++ shapes.py
@@ -0,0 +1,28 @@
+class Rectangle:
+    """A rectangle that can be drawn on the canvas"""
+
+    def __init__(self, x, y, height, width, color):
+        self.x = x
+        self.y = y
+        self.height = height
+        self.width = width
+        self.color = color
+
+    def draw(self, canvas):
+        """Draws itself into the canvas"""
+        # Changes a slice of array with new values
+        canvas.data[self.x: self.x+self.height, self.y: self.y+self.width] = self.color
+
+
+class Square:
+    """A square that can be drawn on the canvas"""
+    def __init__(self, x, y, side, color):
+        self.x = x
+        self.y = y
+        self.side = side
+        self.color = color
+
+    def draw(self, canvas):
+        """Draws itself into the canvas"""
+        # Changes a slice of array with new values
+        canvas.data[self.x: self.x + self.side, self.y: self.y + self.side] = self.color
